4d5f40a7f8273a0f00f042b8f234049b2ab88152
Masaya Sato <sato.masaya.dev@gmail.com>
2019-02-22 10:50:33 +0900

    Rename variable properly

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
+# User-specific setting
+ITERM2_WINDOW_WIDTH = 80
+
 # Error message
 LOAD_ERROR_MESSAGE = "file must be in {0} format".format
-
-# User-specific setting
-WINDOW_WIDTH = 80
