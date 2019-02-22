daf86f5a9657be94d743b1572e9bbd1f544775f3
Masaya Sato <sato.masaya.dev@gmail.com>
2019-02-22 10:20:36 +0900

    Remove apport_python_hook

--- sitecustomize.py
+++ sitecustomize.py
@@ -3,11 +3,5 @@
 
 # The config script is necessary for Python 2.7.
 
-try:
-    import apport_python_hook
-except ImportError:
-    pass
-else:
-    apport_python_hook.install()
 import sys
 sys.setdefaultencoding('utf-8')
