39c859f2d8b8e5fb20ea77adf88b7e74304258bc
Masaya Sato <sato.masaya.dev@gmail.com>
2019-02-22 17:27:55 +0900

    Define the function get_line to fulfill the cell horizontally

--- jupyter/settings.py
+++ jupyter/settings.py
@@ -6,3 +6,8 @@ CWD = Path.cwd()
 # Project root dir will be named notebooks
 PROJECT_ROOT = CWD.joinpath('..', '..').resolve()  
 DATA_DIR = PROJECT_ROOT.joinpath('data')
+
+
+def get_line(num_width, line_type='-'):
+    """Define a horisontal line to fulfill the cell."""
+    return line_type * num_width
