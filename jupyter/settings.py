a4e5aeb506d962c5b88e25ed663dbd2ee8a0655d
Masaya Sato <sato.masaya.dev@gmail.com>
2019-02-27 09:27:46 +0900

    Fix file path properly

--- jupyter/settings.py
+++ jupyter/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 CWD = Path.cwd()
 # Project root dir will be named notebooks
-PROJECT_ROOT = CWD.joinpath('..', '..').resolve()  
+PROJECT_ROOT = CWD.joinpath('..').resolve()  
 DATA_DIR = PROJECT_ROOT.joinpath('data')
 
 
