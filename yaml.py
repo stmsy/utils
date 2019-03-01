3728015fdc0c278092fe1dc68d8f7ec29793a3e4
Masaya Sato <sato.masaya.dev@gmail.com>
2019-03-01 12:13:16 +0900

    Include dot in definition of YAML file extensions; set filepath as pathlib.Path object

--- yaml.py
+++ yaml.py
@@ -5,11 +5,17 @@ import yaml
 from settings import LOAD_ERROR_MESSAGE
 
 
-YAML_EXTS = ['yaml', 'yml']
+YAML_EXTS = ['.yaml', '.yml']
 YAML_LOAD_ERROR_MESSAGE = LOAD_ERROR_MESSAGE("YAML")
 
 
 def load_yaml(filepath):
     """Load the YAML file and return as the Python dictionary."""
-    with open(filepath, 'r') as f:
-        return yaml.load(f)
+    if filepath.suffix in YAML_EXTS:
+        try:
+            with filepath.open('r') as f:
+                return yaml.load(f)
+        raise:
+            Exception("YAML file load failed")
+    else:
+        raise Exception(YAML_LOAD_ERROR_MESSAGE)
