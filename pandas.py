49449328a3d33569626d873b2392cfa41f9a87df
Masaya Sato <sato.masaya.dev@gmail.com>
2019-06-06 15:07:38 +0900

    Give annotations to all the functions

--- pandas.py
+++ pandas.py
@@ -3,17 +3,16 @@
 import pandas as pd
 
 
-def categorize_df(df, target, predictor):
+def categorize_df(df: pd.DataFrame, target: str, predictor: str) -> pd.DataFrame:
     """Categorize the DataFrame object so that column represents the values of
     predictor (categorical) with entries from target values (numerical).
 
     Arguments:
-      df (pandas.DataFrame): the DataFrame object containing categorical
-                             values as columns and numerical values as rows
-      target (str): target variable whose values fill the entries of the
-                    DataFrame object
-      predictor (str): predictor variable whose values correspond to the
-                       column names
+      df: the DataFrame object containing categorical values as columns and
+          numerical values as rows
+      target: target variable whose values fill the entries of the DataFrame
+              object
+      predictor: predictor variable whose values correspond to the column names
 
     Return:
       df_cated (pandas.DataFrame): the DataFrame object with predictor values
@@ -28,44 +27,42 @@ def categorize_df(df, target, predictor):
     return df_cated
 
 
-def get_num_nan_rows(df):
+def get_num_nan_rows(df: pd.DataFrame) -> int:
     """"Get the number of rows filled with missing values.
 
     Argument:
-      df (pandas.DataFrame): the DataFrame object to get the number of rows
-                             filled with missing values
+      df: the DataFrame object to get the number of rows filled with missing
+          values
     Return:
-      _ (int): the number of rows filled with missing values
+      _: the number of rows filled with missing values
     """
     return df.isna().all(axis=1).sum()
 
 
-def is_na_contained(df):
+def is_na_contained(df: pd.DataFrame) -> bool:
     """Check whether the DataFrame object contains a missing value.
 
     Argument:
-      df (pandas.DataFrame): the DataFrame object to be checked whether it
-                             contains a missing value
+      df: the DataFrame object to be checked whether it contains a missing value
 
     Return:
-      True/False (boolean): True if the DataFrame object contains at least
-                            one missing value, False elsewhere
+      True/False: True if the DataFrame object contains at least one missing
+                  value, False elsewhere
     """
     return any(df.isna().any().values)
 
 
-def are_na_contained(df):
-    """Check whether the columns of the DataFrame object contain missing the 
+def are_na_contained(df: pd.DataFrame) -> pd.Series:
+    """Check whether the columns of the DataFrame object contain missing the
     values.
 
     Arguement:
-      df (pandas.DataFrame): the DataFrame object to be checked whether its
-                             columns contain the missing values
+      df: the DataFrame object to be checked whether its columns contain the
+           missing values
 
     Return:
-      sr_na_test_results (pandas.Series): the Series object whose values are
-                                          True or False depending on the
-                                          missing value test
+      sr_na_test_results: the Series object whose values are True or False
+                          depending on the missing value test
     """
     sr_na_test_results = df.isna().any()
     return sr_na_test_results
